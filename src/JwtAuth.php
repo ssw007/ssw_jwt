0a2b5e16513902f753f381906bd864dcd022d01b
ssw <24725867+ssw007@users.noreply.github.com>
2019-04-04 10:20:06 +0800

    修改文件
    修改文件

--- src/JwtAuth.php
+++ src/JwtAuth.php
@@ -53,7 +53,7 @@ class JwtAuth extends JWT
         try {
             JWT::$leeway = 60;
             $tokenInfo=JWT::decode($token, $key, config()['jwt']['algorithms']);
-            return array('code' => 1, 'message' => $tokenInfo');
+            return array('code' => 1, 'message' => $tokenInfo);
         } catch (SignatureInvalidException $e) {//签名不正确
             return array('code' => -1, 'message' => $e->getMessage());
         } catch (BeforeValidException $e) {//签名在某个时间点之后才能用
